72c09a6059827a409d613a94e0924520d47fbda0
Jaguaraquem Adler Reinaldo <jaguar.adler@gmail.com>
2018-11-03 16:28:16 -0300

    Update getMovies to request from swapi.co/api;
    Update getMovies method to request the amount of movies this planets
    appears from https://swapi.co/api.

--- src/main/java/br/com/jadler/models/Planets.java
+++ src/main/java/br/com/jadler/models/Planets.java
@@ -6,6 +6,14 @@ import java.util.Collection;
 import org.springframework.data.annotation.Id;
 import br.com.jadler.annotation.GenerateRepository;
 import br.com.jadler.annotation.GenerateController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.JSONObject;
 
 /**
  *
@@ -99,10 +107,41 @@ public class Planets {
     }
 
     public Integer getMovies() {
-        return movies;
+        if (movies == null && name != null && !name.isEmpty()) {
+            try {
+
+                URL url = new URL("https://swapi.co/api/planets/?format=json&search=" + name);
+                URLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+                conn.setUseCaches(false);
+                conn.setAllowUserInteraction(false);
+
+                InputStream is = conn.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                String result = br.readLine();
+                movies = new JSONObject(result)
+                        .getJSONArray("results")
+                        .getJSONObject(0)
+                        .getJSONArray("films")
+                        .length();
+
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+                movies = null;
+            }
+        }
+
+        return (movies == null) ? 0 : movies;
     }
 
+    /**
+     * This method does nothing and is used just to avoid compilation errors
+     * since the amount of movies is returned from the https://swapi.co/api.
+     *
+     * @param movies
+     */
     public void setMovies(Integer movies) {
-        this.movies = movies;
+        // does nothing
     }
 }
